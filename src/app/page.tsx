c04187b865b99b75ebef7bcafd6ad256687a2594
Emrin <emrin95@gmail.com>
2023-10-30 12:17:52 +0100

    feat(app): add affiliate links

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,5 @@
+import Link from "next/link"
+
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
@@ -58,6 +60,12 @@ export default function Home() {
 
 
       <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
+      </div>
+
+
+      <div className="justify-end">
+        <p>Affiliate links:</p>
+        <Link className="" href="https://vendunt.com/">Vendunt</Link>
       </div>
     </main>
   )
