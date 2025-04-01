9bbeef26de5a742e89b4577e9db4cda83c2ef5e3
Emrin <emrin95@gmail.com>
2025-04-01 17:12:45 +0200

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -21,54 +21,13 @@ export default function Home() {
           <a href="https://www.youtube.com/watch?v=DFFPVsZp4jQ" className="text-blue-300">Presentation</a><br/>
           <a href="https://www.youtube.com/watch?v=-Xxnd54l838" className="text-blue-300">Professionals</a><br/>
           <br />
-          <p><strong>Important Announcement</strong></p>
+          <p><strong>Aclipt Closure Notice</strong></p>
           <br />
-          <p>After much consideration, we have made the difficult decision to close down our online platform. We want to
-            express our sincere gratitude to all our users and supporters for being a part of our journey. Thank you for
-            your trust and support.</p>
-          <br />
-          <p>Effective 30/10/2023, our services will no longer be available. We apologize for any inconvenience this may
-            cause.</p>
-          <p>If you have any questions or need further assistance, please don&apos;t hesitate to <a
-            className="text-blue-300" href="mailto:contact@aclipt.com">contact us</a>. We will do our best to assist you
-            during this transition period.</p>
-          <br />
-          <p>Thank you once again for your support and understanding.</p>
-          <p>- The Aclipt Team</p>
+          <p>As of 30/10/2023, Aclipt has officially shut down. We appreciate the support from our customers and partners over the years. For any inquiries, please contact <a
+            className="text-blue-300" href="mailto:contact@aclipt.com">contact us</a>. Thank you for being part of our journey.
+          </p>
         </div>
 
-        <div className="pt-10">
-          <p><strong>Annonce importante</strong></p>
-          <br />
-          <p>Après mûre réflexion, nous avons pris la difficile décision de fermer notre plateforme en ligne. Nous tenons
-            à exprimer notre sincère gratitude à tous nos utilisateurs et supporters pour avoir fait partie de notre
-            parcours. Merci pour votre confiance et votre soutien.</p>
-          <br />
-          <p>À compter du 30/10/2023, nos services ne seront plus disponibles. Nous nous excusons pour tout inconvénient
-            que cela pourrait causer.</p>
-          <p>Si vous avez des questions ou avez besoin d&apos;aide, n&apos;hésitez pas à <a className="text-blue-300"
-                                                                                            href="mailto:votre@email.com">nous
-            contacter</a>. Nous ferons de notre mieux pour vous aider pendant cette période de transition.</p>
-          <br />
-          <p>Nous vous remercions une fois de plus pour votre soutien et votre compréhension.</p>
-          <p>- L&apos;équipe Aclipt</p>
-
-          <div className="flex justify-center pt-10">
-            <img className="" src="/aclipt.png" alt="Aclipt Logo" width="150" height="150" />
-          </div>
-        </div>
-
-
-      </div>
-
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-      </div>
-
-
-      <div className="justify-end italic">
-        <p>Affiliate links:</p>
-        <Link className="text-blue-300" href="https://vendunt.com/">Vendunt</Link>
       </div>
     </main>
   )
